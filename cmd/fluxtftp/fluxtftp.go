dff234a58b402d1b9d7a59587d95a86d80c75a30
Matti Möll <matti@moell.org>
2014-08-14 12:52:17 +0200

    cmd: cleaned command line interface
    Server is started cleanly now, and it can use systemd socket activation.

--- cmd/fluxtftp/fluxtftp.go
+++ cmd/fluxtftp/fluxtftp.go
@@ -1,15 +1,14 @@
 package main
 
 import (
-	"github.com/mattimo/fluxtftp/client"
-	"github.com/mattimo/fluxtftp/server"
 	"flag"
 	"fmt"
+	"github.com/coreos/go-systemd/activation"
+	"github.com/mattimo/fluxtftp/client"
+	"github.com/mattimo/fluxtftp/server"
 	"os"
 	"path/filepath"
 )
-
-var daemon bool
 
 const (
 	SEARCHPATH      string = "/var/lib/tftpd/"
@@ -24,34 +23,94 @@ var tftpListen string = TFTPDADDR
 var searchPath string = SEARCHPATH
 
 func init() {
+
+	// Evaluate environment
+	daemon_env := os.Getenv("FLUXTFTP_DAEMON")
+	if daemon_env == "true" || daemon_env == "1" {
+		daemon = true
+	} else {
+		daemon = false
+	}
+	tftpListen = os.Getenv("FLUXTFTP_TFTP_LISTEN")
+	searchPath = os.Getenv("FLUXTFTP_SEARCH_PATH")
+
+	// Evalueate Flags
 	flag.Usage = usage
 	const (
-		daemonUsage = "Start in Daemon mode"
+		daemonUsage = "Start Daemon"
+		listenUsage = "tftp Port to listen on"
+		pathUsage   = "file search path"
+		hostUsage   = "fluxtftp host address/socket"
 	)
-	flag.BoolVar(&daemon, "daemon", false, daemonUsage)
-	flag.BoolVar(&daemon, "d", false, daemonUsage+"(shorthand)")
+	flag.BoolVar(&daemon, "daemon", daemon, daemonUsage)
+	flag.BoolVar(&daemon, "d", daemon, daemonUsage+"(shorthand)")
+	flag.StringVar(&tftpListen, "tftp-listen", tftpListen, listenUsage)
+	flag.StringVar(&tftpListen, "t", tftpListen, listenUsage+"(shorthand)")
+	flag.StringVar(&searchPath, "search-path", searchPath, pathUsage)
+	flag.StringVar(&searchPath, "s", searchPath, pathUsage+"(shorthand)")
+	flag.StringVar(&controlAddr, "host", controlAddr, hostUsage)
+	flag.StringVar(&controlAddr, "H", controlAddr, hostUsage+"(shorthand)")
 }
 
-func serverInit() {
+// init the server classically
+func serverInitClassic() string {
+	var control string
 	uid := os.Getuid()
 	if uid == 0 {
-		ControlAddr = "/var/run/fluxtftp/control"
-		err := os.MkdirAll(filepath.Dir(ControlAddr), 0760)
-		fmt.Println("Created control dir:", filepath.Dir(ControlAddr))
+		control = CONTROLSOCK
+		_, err := os.Stat(filepath.Dir(control))
 		if err != nil {
-			panic("Could not create control sock " + filepath.Dir(ControlAddr))
+			err := os.MkdirAll(filepath.Dir(control), 0760)
+			fmt.Println("Created control dir:", filepath.Dir(control))
+			if err != nil {
+				panic("Could not create control sock " + filepath.Dir(control))
+			}
 		}
 	} else {
 		control = fmt.Sprintf(USERCONTROLSOCK, uid)
 		_, err := os.Stat(filepath.Dir(control))
 		if err != nil {
-			panic("Could not create control sock " + filepath.Dir(ControlAddr))
+			err := os.MkdirAll(filepath.Dir(control), 0700)
+			fmt.Println("Created control dir:", filepath.Dir(control))
+			if err != nil {
+				panic("Could not create control sock " + filepath.Dir(control))
+			}
 		}
 	}
+	return control
+}
+
+// use socketactivation return false if value could not be set
+func serverInitSystemd() string {
+	var control string
+	files := activation.Files(true)
+	if len(files) == 0 {
+		return ""
+	}
+	// return only the last file name set
+	for _, f := range files {
+		info, err := f.Stat()
+		if err != nil {
+			continue
+		}
+		control = info.Name()
+	}
+	return control
+}
+
+func serverInit() {
+	if controlAddr != "" {
+		return
+	}
+	controlAddr = serverInitSystemd()
+	if controlAddr == "" {
+		controlAddr = serverInitClassic()
+	}
+
 }
 
 func usage() {
-	fmt.Fprintf(os.Stderr, "Usage of %s [filename]:\n", os.Args[0])
+	fmt.Fprintf(os.Stderr, "Usage of %s [-d] [filename]:\n", os.Args[0])
 	fmt.Fprintf(os.Stderr, " filename: name of the file to be served\n")
 	flag.PrintDefaults()
 }
@@ -63,9 +122,9 @@ func main() {
 		serverInit()
 		fluxftpd, _ := server.NewFluxServer(
 			&server.Config{
-				SearchPath:  SEARCHPATH,
-				TftpAddress: TFTPDADDR,
-				ControlAddr: ControlAddr,
+				SearchPath:  searchPath,
+				TftpAddress: tftpListen,
+				ControlAddr: controlAddr,
 			})
 		go func() {
 			err := fluxftpd.StartControl()
